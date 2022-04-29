a95d5fa6f192ce5dd48bd99aa85a24dfb663dca8
audreybelhoste <audrey.belhoste@gmail.com>
2022-04-29 15:50:35 +0200

    Close modal when click outside or press ESC

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,14 +1,29 @@
+import { useEffect } from 'react';
 import './Modal.css';
 
 const Modal = ({ children, isOpened, onClose, isClosable, style }) => {
+	useEffect(() => {
+		function handleEscapeKey(e) {
+			if (e.code === 'Escape') {
+				onClose();
+			}
+		}
+	
+		document.addEventListener('keydown', handleEscapeKey)
+		return () => document.removeEventListener('keydown', handleEscapeKey)
+  	}, [])
 
     return (
 		isOpened && (
-			<div class="modal">
-				<div class="modal-content" style={style}>
+			<div className="modal" onClick={onClose}>
+				<div 
+					className="modal-content" 
+					style={style} onClick={e => {
+          			e.stopPropagation();
+        		}}>
 					{isClosable && 
-						<button class="modal-close" onClick={onClose}>
-							<span class="modal-sr-only">Close</span>
+						<button className="modal-close" onClick={onClose}>
+							<span className="modal-sr-only">Close</span>
 						</button>
 					}
 					<div>
